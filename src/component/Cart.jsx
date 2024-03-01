bcc074d4fc3ff9fcf085ccbc1b407e4eb6a09fcf
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-01 07:57:18 +0100

    added more data and added useReducer

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,7 +1,20 @@
 import React, { useState, useEffect, useReducer } from 'react'
+import { phonesList } from './shared/dataList.js'
+
+const reducer = (state, action) => {}
 
 const Cart = () => {
-  return <div>Cart</div>
+  const [dataList, setDataList] = useState(phonesList)
+
+  const [state, dispatch] = useReducer(reducer, dataList)
+
+  return (
+    <div>
+      {state.map((item) => (
+        <div>{item.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export default Cart
